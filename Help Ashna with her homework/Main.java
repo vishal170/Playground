cf113b4a64a8123937e5b223356c2fd3164ab733
vishal170 <50742340+vishal170@users.noreply.github.com>
2020-04-23 19:21:58 +0530

    Test code pushed to repository.

---
+++ Help Ashna with her homework/Main.java
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cin>>a;
+  cin>>b;
+  int c=a+b;
+  int d=a-b;
+  int e=a*b;
+  int f=a/b;
+  int g=a%b;
+  cout<<"a+b="<<c<<endl;
+  cout<<"a-b="<<d<<endl;
+  cout<<"a*b="<<e<<endl;
+  cout<<"a/b="<<f<<endl;
+  cout<<"a%b="<<g<<endl;
+return 0;
+}
+  
